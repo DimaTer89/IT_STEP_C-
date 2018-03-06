5109166dbfa74046b492e0d1ddf659c9f0f07c97
DimaTer89 <35215775+DimaTer89@users.noreply.github.com>
2018-03-06 21:18:03 +0300

    7-3-1.cpp

---
+++ 7-3-1.cpp
@@ -0,0 +1,29 @@
+/*Даны два целых числа А и В. Вывести все числа от А до В в порядке возрастания, если А<В, или в порядке убывания в противном случае. Пример: ввод: 5 1 вывод: 5 4 3 2 1.*/
+#include <iostream>
+using namespace std;
+void vyvod(int a, int b) {
+	if (a == b) {
+		cout << b;
+		return;
+	}
+	
+	if (a < b) {
+		vyvod(a+1,b);
+		cout <<" " << a ;
+	}
+	if (a > b) {
+		vyvod(b,a-1);
+		cout <<" "<< a;
+	}
+}
+void main() {
+	setlocale(LC_ALL, "rus");
+	int num1, num2;
+	cout << " Введите число А :";
+	cin >> num1;
+	cout << " Введите число B :";
+	cin >> num2;
+	vyvod(num1, num2);
+	cout << endl;
+	system("pause");
+}
