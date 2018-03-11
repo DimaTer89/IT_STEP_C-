ea42ec4490b1edb39a1627d3ad6d34e77872ff6f
DimaTer89 <35215775+DimaTer89@users.noreply.github.com>
2018-03-11 17:47:45 +0300

    Update 7-3-8.cpp

--- 7-3-8.cpp
+++ 7-3-8.cpp
@@ -1,7 +1,16 @@
 //Написать рекурсивную функцию, которая принимает двумерный массив целых чисел и количество сдвигов и выполняет круговой сдвиг массива вправо.
 #include <iostream>
 #include <time.h>
+#define ROW 3
+#define COL 4
 using namespace std;
+void randArr(int *a, int n, int m) {
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++, a++) {
+			*a = rand() % 21 - 10;
+		}
+	}
+}
 void printArr(int *a, int n,int m) {
 	for (int i = 0; i < 3; i++) {
 		for (int j = 0; j < 4; j++, a++) {
@@ -9,42 +18,34 @@ void printArr(int *a, int n,int m) {
 		}
 		cout << endl;
 	}
-	cout << endl;
+	
 }
-void shift(int n, int a[][4], int size) {
-	int k = 0;
+void shift(int x,int n, int a[][COL], int size) {
+	int kol = n;
+	int score = x;
 	for (int i = 0; i < size; i++) {
-		for (int j = 1; j < 4; j++) {
+		for (int j = 1; j < COL; j++) {
 			int tmp = a[i][j];
 			a[i][j] = a[i][j - 1];
 			a[i][j - 1] = tmp;
 		}
 	}
-	k++;
-	if(k<n)shift(k,a,3);
-}
-void print(int *a, int size) {
-	for (int i = 0; i < size; i++, a++) {
-		cout << " " << *a;
-	}
-	cout << endl;
+	score++;
+	if(score<kol)shift(score,kol,a,ROW);
 }
 void main() {
 	setlocale(LC_ALL, "rus");
 	srand(time(0));
-	//int arr1[4] = { 4,5,6,7 };
-	//cout << " Одномерный массив \n";
-	//print(arr1, 4);
+	int arr[ROW][COL];
 	int num;
-	//int *pPtr = arr1 + 1;
+	int count = 0;
+	cout << " Двумерный массив \n";
+	randArr(&arr[0][0], ROW, COL);
+	printArr(&arr[0][0], ROW, COL);
 	cout << " Введите количество сдвигов : ";
 	cin >> num;
-	//shift(num,pPtr, 4);
-	//print(arr1, 4);
-	int arr[3][4] = { 4,5,6,7,1,2,4,1,4,5,6,9 };
-	cout << " Двумерный массив \n";
-	printArr(&arr[0][0], 3, 4);
-	shift(num,arr,3);
-	printArr(&arr[0][0], 3, 4);
+	cout << " Изменённый двумерный массив :\n";
+	shift(count,num,arr,3);
+	printArr(&arr[0][0], ROW, COL);
 	system("pause");
 }
