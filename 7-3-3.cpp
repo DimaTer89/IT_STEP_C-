f9ba75b88a309f38c7ae2c252b33e38c0d96d974
DimaTer89 <35215775+DimaTer89@users.noreply.github.com>
2018-03-13 20:32:19 +0300

    Update 7-3-3.cpp

--- 7-3-3.cpp
+++ 7-3-3.cpp
@@ -1,20 +1,15 @@
 /*Дано натуральное число N. Вычислить сумму его цифр. Нельзя использовать массивы и циклы!*/
 #include <iostream>
 using namespace std;
-int sum(int a, int *ptr) {
-	if (a / 10) {
-		*ptr += a % 10;
-		a /= 10;
-		sum(a, ptr);
-	}
-	else return *ptr += a;
+int sum(int a) {
+	if (a < 10)return a;
+	return sum(a / 10) + a % 10;
 }
 void main() {
 	setlocale(LC_ALL, "rus");
 	int chis;
-	int cif = 0;
 	cout << " Введите число : ";
 	cin >> chis;
-	cout << " Сумма цифр числа = " << sum(chis,&cif) << endl;
+	cout << " Сумма цифр числа = " << sum(chis) << endl;
 	system("pause");
 }
