1669ffb12ac264de37abf652d145bc22a2d58ea9
DimaTer89 <35215775+DimaTer89@users.noreply.github.com>
2018-03-15 20:02:33 +0300

    Create kontr.cpp

---
+++ kontr.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <time.h>
+#define N 10
+#define ROW 4
+#define COL 7
+using namespace std;
+bool simple(int a) {
+	int b = a / 2;
+	bool flag = false;
+	int i = 2;
+	while (i<b)
+	{
+		if ((a%i) == 0)
+		{
+			flag = true;
+			break;
+		}
+		i++;
+	}
+	return flag;
+}
+void randArr(int *a, int size) {
+	for (int i = 0; i < size; i++, a++) {
+		*a = rand()%101;
+	}
+}
+void printArr(int *a, int size) {
+	for (int i = 0; i < size; i++, a++) {
+		cout << " " << *a;
+	}
+	cout << endl;
+}
+int kolsimple(int *a, int size) {
+	int kol = 0;
+	for (int i = 0; i < size; i++, a++) {
+		if (!simple(*a))kol++;
+	}
+	return kol;
+}
+void randArr(int *a, int n, int m) {
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++, a++) {
+			*a = rand() % 101;
+		}
+	}
+}
+void printArr(int *a, int n, int m) {
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++, a++) {
+			printf(" %3d ", *a);
+		}
+		cout << endl;
+	}
+
+}
+void kolsimple(int *a, int n, int m) {
+
+}
+void main() {
+	setlocale(LC_ALL, "rus");
+	srand(time(0));
+	int twoArr[ROW][COL];
+	cout << " Количество простых чисел в массиве = " << kolsimple(arr, N) << endl;
+	cout << " Двумерный массив \n";
+	randArr(&twoArr[0][0], ROW, COL);
+	printArr(&twoArr[0][0], ROW, COL);
+	for (int i = 0; i < ROW; i++) {
+		cout << " Простые числа для " << i + 1 << " строки = " << kolsimple(twoArr[0] + i*COL, COL) << endl;
+	}
+	system("pause");
+}
