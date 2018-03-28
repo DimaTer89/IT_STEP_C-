79a7f31f896c57468d56683f142dfff56cb5edf5
DimaTer89 <35215775+DimaTer89@users.noreply.github.com>
2018-03-28 20:37:37 +0300

    Update 8-1-6.cpp

--- 8-1-6.cpp
+++ 8-1-6.cpp
@@ -75,3 +75,72 @@ void main() {
 	delete[]mas;
 	system("pause");
 }
+====================================================
+	/*Написать функцию, которая уменьшает размер двумерной матрицы, удаляя из нее столбец. Измененная матрица должна занимать ровно столько памяти, сколько необходимо!
+Проиллюстрировать работу этой функции:
+1) Заполнить динамический двумерный массив случайными числами (размерность матрицы запросить у пользователя).
+2) Запросить номер столбца, который будет удален.
+3) Удалить столбец
+5) Распечатать преобразованную матрицу.*/
+#include <iostream>
+#include <time.h>
+using namespace std;
+void randArr(int **mas, int row, int col) {
+	for (int i = 0; i < row; i++) {
+		for (int j = 0; j < col; j++) {
+			mas[i][j] = rand() % 101 - 50;
+		}
+	}
+}
+void printArr(int **mas, int row, int col) {
+	for (int i = 0; i < row; i++) {
+		for (int j = 0; j < col; j++) {
+			cout << mas[i][j] << "\t";
+		}
+		cout << endl;
+	}
+	cout << endl;
+}
+void udalCol(int **mas, int row, int &col, int stolb) {
+	
+	for (int i = 0; i < row; i++) {
+		int * tmp = new int[col - 1];
+		for (int j = 0; j < stolb; j++) 
+				tmp[j] = mas[i][j];
+		for (int j = stolb + 1; j < col; j++)
+			tmp[j - 1] = mas[i][j];
+		delete[]mas[i]; 
+		mas[i] = tmp;
+	}
+	col -= 1;
+}
+void main() {
+	setlocale(LC_ALL, "rus");
+	int row, col, stolb;
+	srand(time(0));
+	cout << " Введите количество строк : ";
+	cin >> row;
+	cout << " Введите количество столбцов : ";
+	cin >> col;
+	int **mas;
+	mas = new int*[row];
+	for (int i = 0; i < row; i++) {
+		mas[i] = new int[col];
+	}
+	randArr(mas, row, col);
+	cout << " Исходный массив \n";
+	printArr(mas, row, col);
+	do {
+		cout << " Введите номер столбца ,который нужно удалить : ";
+		cin >> stolb;
+		if (stolb<1 || stolb>col)cout << " Ошибка! Введите снова : ";
+	} while (stolb<1 || stolb>col);
+	udalCol(mas, row, col, stolb);
+	cout << " Преобразованный массив \n";
+	printArr(mas, row, col);
+	for (int i = 0; i < row; i++) {
+		delete mas[i];
+	}
+	delete[]mas;
+	system("pause");
+}
