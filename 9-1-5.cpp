619236b4b52b4ab5dd1342afbe3c91bf0d81dceb
DimaTer89 <35215775+DimaTer89@users.noreply.github.com>
2018-04-05 21:06:10 +0300

    Update 9-1-5.cpp

--- 9-1-5.cpp
+++ 9-1-5.cpp
@@ -1,27 +1,46 @@
 //Вводится строка, которая состоит из слов, разделенных одним или несколькими пробелами. Поменять местами первое и последнее слово.
 #include <iostream>
-#include <Windows.h>
 using namespace std;
-
-void main() {
-	SetConsoleCP(1251);
-	SetConsoleOutputCP(1251);
-	const int N = 256;
-	char string[N];
-	cout << " Введите строку : ";
-	gets_s(string, N);
-	char* buf;
-	char* ptr = strtok_s(string, " ", &buf);
-	char* first;
-	first = ptr;
-	cout << " Первое слово " << first << endl;
-	char*last;
-	while (ptr != NULL) {
-		last = ptr;
-		ptr = strtok_s(NULL, " ", &buf);
+char* fis(char*str) {
+	while (*str == ' ')str++;
+	if (*str == '\0')return NULL;
+	while (*str != ' '&&*str != '\0') {
+		str++;
 	}
-	cout << " Последнее слово : " << last << endl;
-	
-	cout << string << endl;
+	return str-1;
+}
+char* las(char*str) {
+	char* tmp = str + strlen(str)-1;
+	while (*tmp == ' ')tmp--;
+	while (*tmp != ' ') {
+		tmp--;
+	}
+	return tmp+1;
+}
+void mycat(char *&str,char *ptr, char*endptr) {
+	while (ptr<endptr) {
+		*str++ = *ptr++;
+	}
+}
+void convert(char *s, char*s1, char*str,char*str1) {
+	char *tmp = str1;
+	mycat(tmp, s1, str + strlen(str));
+	mycat(tmp, (s + 1), (s1 - 1));
+	mycat(tmp,str,(s+1));
+	*tmp = '\0';
+}
+void main() {
+	setlocale(LC_ALL, "rus");
+	const int N = 256;
+	char str[N];
+	char str1[N];
+	cout << " Введите строку : ";
+	gets_s(str, N);
+	cout << " Исходная строка : \n";
+	cout << str << endl;
+	char* first = fis(str);
+	char* last = las(str);
+	convert(first, last, str, str1);
+	puts(str1);
 	system("pause");
 }
