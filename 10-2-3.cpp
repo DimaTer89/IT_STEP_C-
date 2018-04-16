81b97c7b06c265508af20a69a387f1d7b7485c1b
DimaTer89 <35215775+DimaTer89@users.noreply.github.com>
2018-04-16 19:08:45 +0300

    Update 10-2-3.cpp

--- 10-2-3.cpp
+++ 10-2-3.cpp
@@ -1,5 +1,5 @@
 /*Объявите объединение floatingPoint с элементами float f, double d и long double l. Создайте три переменные типа floatingPoint.
-Введите в них три значения: a) типа float, б) типа double, в) типа long double. 
+Введите в них три значения: a) типа float, б) типа double, в) типа long double.
 Каждая переменная-объединение должна быть распечатана как float, как double, как long double. Всегда ли значения печатаются правильно?*/
 #include <iostream>
 using namespace std;
@@ -16,14 +16,14 @@ void main() {
 	cout << " Введите число типа float : ";
 	cin >> num1;
 	FloatPoint.f = num1;
+	cout << " Введённое число в виде типа float = " << FloatPoint.f << endl;
 	cout << " Введите число типа double : ";
 	cin >> num2;
 	FloatPoint.d = num2;
+	cout << " Введённое число в виде типа double = " << FloatPoint.d << endl;
 	cout << " Введите число типа long double : ";
 	cin >> num3;
 	FloatPoint.l = num3;
-	cout << " Введённое число в виде типа float = " << FloatPoint.f << endl;
-	cout << " Введённое число в виде типа double = " << FloatPoint.d << endl;
-	cout << " Введённое число в виде типа long double = " <<FloatPoint.l << endl;
+	cout << " Введённое число в виде типа long double = " << FloatPoint.l << endl;
 	system("pause");
 }
